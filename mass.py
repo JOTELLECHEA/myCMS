a40b57831355c0c2ffed2eed29878eca3355951c
jonathanTellechea <jtellece@ucsc.edu>
2022-07-24 19:36:34 -0400

    upgrade

--- mass.py
+++ mass.py
@@ -1,21 +1,29 @@
 #Program that outputs a histogram of the Zee_0 invariant mass
 from ROOT import TCanvas,TH1F
 import ROOT
-import math
+import sys
+import time
+
+fileName = sys.argv[1]
+
+print('Opening:',fileName,'\n')
+
 
 #opens the root file
-f = ROOT.TFile("myoutput.root")
+f = ROOT.TFile(fileName)
 
 #loads TTree into the memory
-MyTree = f.Get("myelectrons/Events")
+MyTree = f.Get('myelectrons/Events')
+print('Found myelectron TTree.\n')
 
 #create a empty histogram
-h2 = ROOT.TH1D("mass","mass;Invariant Mass[GeV];Events/Bin",120,0,120)
+h = ROOT.TH1D('mass','mass;Invariant Mass[GeV];Events/Bin',120,0,120)
 
 #loop through the entries of the leaf and fill the histogram with data
 #then draw histogram
 
 entries = MyTree.GetEntries()
+print('Found',entries,'envents.\n')
 
 #declare variables
 
@@ -31,7 +39,11 @@ for event in MyTree:
    if event.numberelectron == 2:
        lep4vec[0].SetPtEtaPhiM(event.electron_pt[0],event.electron_eta[0],event.electron_phi[0],0)
        lep4vec[1].SetPtEtaPhiM(event.electron_pt[1],event.electron_eta[1],event.electron_phi[1],0)
-       h2.Fill((lep4vec[0]+lep4vec[1]).M())
+       h.Fill((lep4vec[0]+lep4vec[1]).M())
 
-h2.Draw()
+histCount = int(h.GetEntries())
+print('There are',histCount,'events with two electrons.\n')
+print('Creating Invariant Mass Plot.')
+time.sleep(3)
+h.Draw()
 
